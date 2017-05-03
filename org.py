c2e225f25979aedc7d0d3c1d5babbd070b4c56ba
hrehfeld <git@haukerehfeld.de>
2017-05-04 00:58:07 +0200

    chg: working

--- org.py
+++ org.py
@@ -11,6 +11,16 @@ import operator
 import sys
 from pprint import pprint
 
+import re
+
+def unique(l):
+    T = type(l)
+    r = T()
+    for e in l:
+        if e not in r:
+            r += T(e)
+    return r
+
 #unicodePrintables = ''.join(chr(c) for c in range(sys.maxunicode) 
 #                            if not chr(c).isspace())
 
@@ -30,7 +40,7 @@ saction = nullDebugAction, _defaultSuccessDebugAction, nullDebugAction
 def Syntax():
     comment = '# ' + restOfLine
     ParserElement.setDefaultWhitespaceChars('')
-    ParserElement.enablePackrat()
+    #ParserElement.enablePackrat()
 
     def lax(p, strict=None):
         '''support less strict syntax'''
@@ -117,10 +127,14 @@ def Syntax():
 
     title = SkipTo(tags | eol).setName('title')
 
+    priority = enclosed(s('#') + just_chars('A-Z'), s('['), s(']'))
+
     headline_token = Literal('*')
     headline_start = lambda depth=(1,None): Combine(headline_token * depth) + swsl1
     headline = lambda depth=(1,None): line((
-        headline_start(depth) + Optional(lax(owsl) + todo_keyword + swsl1)
+        headline_start(depth)
+        + Optional(lax(owsl) + todo_keyword + swsl1)
+        + Optional(lax(owsl) + priority + swsl1)
         + title + Optional(tags.copy()))
     ).setName('headline')
 
@@ -173,14 +187,11 @@ def Syntax():
         withws = lambda a, b, c: a + owsl + b + owsl + c
         return line(owsl + (
             withws(closed, scheduled, o(deadlined))
-            | withws(closed, deadlined, o(scheduled)).setName('closed deadline').setDebug()
+            | withws(closed, o(deadlined), o(scheduled)).setName('closed deadline')#.setDebug()
             | withws(scheduled, deadlined, o(closed))
-            | withws(scheduled, closed, o(deadlined))
+            | withws(scheduled, o(closed), o(deadlined))
             | withws(deadlined, closed, o(scheduled))
-            | withws(deadlined, scheduled, o(closed))
-            | closed
-            | scheduled
-            | deadlined
+            | withws(deadlined, o(scheduled), o(closed))
             ) + owsl).setName('named_dates')
 
 
@@ -192,7 +203,7 @@ def Syntax():
     drawer_entry = Group(~drawer_end + drawer_keyword(SkipTo(drawer_token) | LineEnd()))
 
     drawer = lambda name, entries: Group(
-        line(lax(owsl) + drawer_keyword(name)).setName('drawer_start: ' + name).setDebug()
+        line(lax(owsl) + drawer_keyword(name)).setName('drawer_start: ' + name)#.setDebug()
         + entries
         + line(lax(owsl) + drawer_end))
     
@@ -227,13 +238,31 @@ def Syntax():
     ).setName('link')
 
     
-    punctuationc = '-.,;:"\'/'
+    punctuationc = '-,;:"\'/.[](){}<>'
     punctuation = just_chars(punctuationc)
     opunctuation = just_chars(punctuationc, '*')
-    markup = '-*/_=~+'
-    line_start_chars = markup + '#\d|:'
+    inline_markup = '-*/_=~+'
+    text_markup = '|' + inline_markup
+    line_start_chars = inline_markup + '#\d|:'
 
-    word = CharsNotIn(wsc + punctuationc, min=1).setName('word')#.setDebugActions(*saction)
+    slurpc = unique((text_markup + punctuationc))
+    def for_re_brackets(s):
+        r = ''
+        e = ''
+        for c in s:
+            if c == '-':
+                e = '-'
+            elif c in ']':
+                r += '\\' + c
+            else:
+                r += c
+        r = r + e
+        return r
+    slurpre = '[^\n%s]+' % for_re_brackets(slurpc)
+    #print(repr(slurpre))
+    slurp = Regex(slurpre).setName('slurp')#.setDebug()#.setDebugActions(*saction)
+
+    word = Regex('\S+').setName('word')#.setDebug()
 
     word_line = Forward().setName('word_line')
     #nested tags not supported
@@ -261,20 +290,25 @@ def Syntax():
 
     #TODO
     naive_line = lambda p: (LineStart() + p + LineEnd())#.setDebug().setDebugActions(nullDebugAction, _defaultSuccessDebugAction, nullDebugAction)
-    simple_fast_line = naive_line(Regex('[^%s\n]*' % (line_start_chars))).setName('simple_fast_line')
-    fast_line = naive_line(Regex('[ \t]*[^%s\n][^%s\n]*' % (line_start_chars, markup))).setName('fast_line')
+    simple_fast_line = (LineStart() + Regex('[^%s\n]*\n' % (line_start_chars))).setName('simple_fast_line')
+    #fast_line = naive_line(Regex('[ \t]*[^%s\n][^%s\n]*' % (line_start_chars, inline_markup))).setName('fast_line')
     fast_lines = simple_fast_line
     fast_word = Regex('[^%s\n]+' % (line_start_chars)).setName('fast_word')#.setDebugActions(*saction)
     complex_words = [any_date_range(), link]
-    def make_word_line(*filter_by):
-        words = [nonnested_tag(d).setName('Tag %s' % d) for d in markup]
-        words = [fast_word] + complex_words + words + [word]
-        words = [p for p in words if p not in filter_by]
+
+    inline_tags = [nonnested_tag(d).setName('Tag %s' % d) for d in inline_markup]
+
+    def words(*filter_by):
+        words = complex_words + inline_tags + [word]
+        #words = [p for p in words if p not in filter_by]
         #words = [p.setDebugActions(*saction) for p in words]
-        words = [opunctuation + p + opunctuation for p in words]
-        words += [(wsl + punctuation + FollowedBy(wsl)).setName('just_punctuation').setDebugActions(*saction)]
-        return line(~para_illegal_start + OneOrMore(owsl + MatchFirst(words)) + owsl)
-    word_line << (fast_lines | make_word_line().setParseAction(join_words)).setName('word_line')
+        #words = [opunctuation + p + opunctuation for p in words]
+        #words += [(wsl + punctuation + FollowedBy(wsl)).setName('just_punctuation').setDebugActions(*saction)]
+        return ~para_illegal_start + OneOrMore(slurp | MatchFirst(words))
+
+    word_line << (fast_lines
+                  | line(words().setName('words inner')).setParseAction(join_words).setName('words')#.setDebug()
+    ).setName('word_line')
 
 
     text = (OneOrMore(word_line)).setName('text').setParseAction(join_words)#.setDebug()
@@ -336,9 +370,9 @@ def Syntax():
         r = Group(headline((min_depth, None)) + oeols
                   + Optional(Group(named_dates())) + oeols
                   + Optional(prop_drawer) + oeols
-                  + Optional(logbook_drawer)
-                  + Optional(oeols + Group(content) + oeols)
-                  + nested + oeols
+                  + Optional(logbook_drawer) + oeols
+                  + Optional(Group(content) + oeols)
+#                  + nested + oeols
         )
         return r
 
@@ -361,9 +395,21 @@ if __name__ == '__main__':
 
     p = ArgumentParser('orgmode')
     p.add_argument('file')
+    p.add_argument('--profile', action='store_true')
+
 
     args = p.parse_args()
     filename = args.file
     with open(filename, 'r') as f:
-        ast = parse(f.read())
-    pprint(ast.asList())
+        s = f.read()
+
+    if args.profile:
+        import profile
+        profile.run('parse(s)', sort='cumtime')
+    else:
+        import time
+        start_time = time.time()
+        ast = parse(s)
+        duration = time.time() - start_time
+        pprint(ast.asList())
+        print(duration)
