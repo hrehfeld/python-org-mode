6db6accc31cbb0c726e8ab3def5ebe275c8fd6f7
hrehfeld <git@haukerehfeld.de>
2017-05-14 01:16:11 +0200

    cln: str_build had re stuff left

--- str_build.py
+++ str_build.py
@@ -1,28 +1,4 @@
-enclosed = lambda x, s, e=None: s + x + (s if e is None else e)
-
-i_ = lambda s: r'(?i:%s)' % s
-g_ = lambda s: r'(?:%s)' % s
-or_ = lambda *args: g_('|'.join(args))
-in_ = lambda *args: r'[%s]' % (''.join(args))
-def re_count(s):
-    if isinstance(s, str):
-        assert(s in '*+?')
-        return s
-    elif isinstance(s, int):
-        return '{%s}' % s
-    return '{%s,%s}' % s
-n_ = lambda s,n='+': g_(s) + re_count(n)
-o = lambda s: g_(s) + '?'
-g = lambda n, s: r'(?P<%s>%s)' % (n, s)
-ref_ = lambda name: r'(?P=%s)' % name
-ref_yes_no_ = lambda name, yes, no: r'(?(%s)%s|%s)' % (name, yes, no)
-def careful(s):
-    assert(s[-1] in '+*')
-    return g_(s) + '?'
-
-lax = lambda lax, strict=None: lax if strict is None else or_(strict, lax) 
-lax_only = lambda lax, strict=None: lax
-
+from re_parse import enclosed
 
 eol = '\n'
 ws1 = ' '
