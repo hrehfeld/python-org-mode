7cc099b512c2d04289813e75139f628e27a8d234
hrehfeld <git@haukerehfeld.de>
2017-05-14 18:29:06 +0200

    cln: assert list type

--- ast.py
+++ ast.py
@@ -145,6 +145,7 @@ class Block:
         self.name = name
         self.value = value
         self.content = content or ''
+        assert(isinstance(self.content, list))
 
     def __repr__(self):
         return repr([type(self).__name__, [self.name, self.value], *self.content])
